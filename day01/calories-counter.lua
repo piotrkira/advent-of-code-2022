9580b1b180023b182cbc18c3574c8b1bb00ce899
Piotr Kira <piotrakira@gmail.com>
2022-12-13 21:56:20 +0100

    refactor: save max calories after counting all of one group

--- day01/calories-counter.lua
+++ day01/calories-counter.lua
@@ -3,12 +3,12 @@ local counter = 0
 
 for line in io.lines("input.txt") do
   if line == '' then
-    counter = 0
-  else
-    counter = counter + line
     if counter > max then
       max = counter
     end
+    counter = 0
+  else
+    counter = counter + line
   end
 end
 
