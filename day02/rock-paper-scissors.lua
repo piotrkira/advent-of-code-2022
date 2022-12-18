ccc700d2be98f575b525c58237f1cf010f151a95
Piotr Kira <piotrakira@gmail.com>
2022-12-18 17:35:22 +0100

    feat: calculate score based on "ultra top secret" strategy

--- day02/rock-paper-scissors.lua
+++ day02/rock-paper-scissors.lua
@@ -8,3 +8,13 @@ for line in io.lines("input.txt") do
 end
 
 print(score)
+
+-- part two --
+score = 0
+local should_do_table = { A = { X = 3, Y = 4, Z = 8}, B = { X = 1, Y = 5, Z = 9}, C = { X = 2, Y = 6, Z = 7}}
+for line in io.lines("input.txt") do
+  local oponent = line:sub(1,1)
+  local should_do = line:sub(3,3)
+  score = score + should_do_table[oponent][should_do]
+end
+print(score)
